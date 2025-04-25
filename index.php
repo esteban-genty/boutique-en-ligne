14175bbe80db38d3995ebca93aab2712c6737e8d
sebastien-liveyupeng <sebastien.live-yu-peng@laplateforme.io>
2025-04-25 16:13:54 +0200

    header / footer ok / responsive header burger menu

--- index.php
+++ index.php
@@ -1,22 +1,43 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>OMNI - boutique en ligne</title>
+    <title>OMNI</title>
+ <link rel="stylesheet" href="../boutique-en-ligne/public/assets/css/header.css">
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 
-    <!--------------------STYLE----------------------->
-
-
-
-
-
-
-
-    <!------------------sSCRIPT----------------------->
 </head>
 <body>
-    
+   
+<header>
+  <div class="header-container">
+    <div class="left-group">
+      <div class="logo">OMNI</div>
+      <button class="burger" aria-label="Menu" aria-expanded="false">
+        <span></span><span></span><span></span>
+      </button>
+      <nav class="main-nav">
+        <ul>
+          <li><a href="#">Homme</a></li>
+          <li><a href="#">Femme</a></li>
+        </ul>
+      </nav>
+    </div>
+
+    <div class="search-wrapper">
+      <input type="text" placeholder="Rechercher" aria-label="Recherche"/>
+    </div>
+
+    <div class="icons">
+      <a href="#" aria-label="Panier"><i class="fa-solid fa-cart-shopping"></i></a>
+      <a href="#" aria-label="Mon compte"><i class="fa-solid fa-user"></i></a>
+    </div>
+  </div>
+</header>
+
+
+
 </body>
 </html>
