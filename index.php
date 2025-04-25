ec657ee88b4338be8c1c35415ed448b639b6f56d
sebastien-liveyupeng <sebastien.live-yu-peng@laplateforme.io>
2025-04-25 16:13:56 +0200

    header/footer ok

--- index.php
+++ index.php
@@ -5,6 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>OMNI</title>
  <link rel="stylesheet" href="../boutique-en-ligne/public/assets/css/header.css">
+ <link rel="stylesheet" href="../boutique-en-ligne/public/assets/css/footer.css">
+
+
+
+
  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 
@@ -37,7 +42,57 @@
   </div>
 </header>
 
-
+<footer>
+    <div class="footer-top">
+      <div class="footer-logo">
+        <h2>nom du site |</h2>
+      </div>
+      <div class="footer-links">
+        <h3>Informations Légales</h3>
+        <ul>
+          <li><a href="#">Charte de Confidentialités</a></li>
+          <li><a href="#">Mention Légales</a></li>
+          <li><a href="#">Conditions générales de ventes</a></li>
+        </ul>
+      </div>
+      <div class="footer-links">
+        <h3>Informations Légales</h3>
+        <ul>
+          <li><a href="#">Charte de Confidentialités</a></li>
+          <li><a href="#">Mention Légales</a></li>
+          <li><a href="#">Conditions générales de ventes</a></li>
+        </ul>
+      </div>
+      <div class="footer-brand">
+        <h3>La marque</h3>
+        <ul>
+          <li><a href="#">Nom de marque</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="footer-contact-social">
+      <div class="footer-contact">
+        <h3>Contactez-nous</h3>
+        <form>
+          <label for="email">Mon email :</label>
+          <input type="email" id="email" placeholder="Votre email">
+          <label for="message">Mon message :</label>
+          <input type="text" id="message" placeholder="Votre message">
+          <button type="submit" class="send-button"><i class="fas fa-arrow-right"></i></button>
+        </form>
+      </div>
+      <div class="footer-social">
+        <a href="#"><i class="fab fa-instagram"></i></a>
+        <a href="#"><i class="fab fa-twitter"></i></a>
+        <a href="#"><i class="fab fa-facebook-f"></i></a>
+        <a href="#"><i class="fab fa-pinterest"></i></a>
+        <a href="#"><i class="fab fa-snapchat-ghost"></i></a>
+      </div>
+    </div>
+    <div class="footer-bottom">
+      <p>&copy; 2025 Copyright Sébastien, Esteban, Lamine</p>
+    </div>
+  </footer>
 
 </body>
 </html>
