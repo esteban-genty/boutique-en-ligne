5d330ea838d945e4cd2a3d05d4d5343080f1aca0
Lamine Dxallo <mamadou-lamine.diallo@laplateforme.io>
2025-04-26 11:59:28 +0200

    Create User model for login feature

---
+++ app/models/User.php
@@ -0,0 +1,15 @@
+<?php
+
+class User {
+    private $db;
+
+    public function __construct() {
+        $this->db = new PDO('mysql:host=127.0.0.1;port=3306;dbname=omni;charset=utf8', 'root', 'root');
+    }
+
+    public function findByEmail($email) {
+        $stmt = $this->db->prepare('SELECT * FROM account WHERE mail = :email');
+        $stmt->execute(['email' => $email]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+}
