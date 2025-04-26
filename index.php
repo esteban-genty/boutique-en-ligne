2ca8b2b4987b528f39fa32e616aadf772d22c52d
Lamine Dxallo <mamadou-lamine.diallo@laplateforme.io>
2025-04-26 14:05:28 +0200

    feat(router): add simple router handling login and logout routes

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+require_once 'app/controllers/AuthController.php';
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+switch ($uri) {
+  case '/boutique-en-ligne/':
+  case '/boutique-en-ligne':
+    header('Location: /boutique-en-ligne/login');
+    exit;
+  case '/boutique-en-ligne/login':
+    $controller = new AuthController();
+    $controller->login();
+    break;
+  case '/boutique-en-ligne/logout':
+    $controller = new AuthController();
+    $controller->logout();
+    break;
+  default:
+    echo "404 Not Found";
+    break;
+}
