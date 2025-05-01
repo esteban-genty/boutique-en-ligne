b7c56d666d8bf8ab74bbc815b9765822dcc86bac
Lamine Dxallo <mamadou-lamine.diallo@laplateforme.io>
2025-05-01 11:44:05 +0200

    add namespace

--- app/models/User.php
+++ app/models/User.php
@@ -1,19 +1,21 @@
 <?php
 
+namespace App\Models;
+
 class User
 {
   private $db;
 
   public function __construct()
   {
-    $this->db = new PDO('mysql:host=127.0.0.1;port=3306;dbname=omni;charset=utf8', 'root', 'root');
+    $this->db = new \PDO('mysql:host=127.0.0.1;port=3306;dbname=omni;charset=utf8', 'root', 'root');
   }
 
   public function findByEmail($email)
   {
     $stmt = $this->db->prepare('SELECT * FROM account WHERE email = :email');
     $stmt->execute(['email' => $email]);
-    return $stmt->fetch(PDO::FETCH_ASSOC);
+    return $stmt->fetch(\PDO::FETCH_ASSOC);
   }
 
   public function create($data)
