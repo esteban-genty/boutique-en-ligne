11ca4b0a7930955323c514405bb389944d7564ed
Lamine Dxallo <mamadou-lamine.diallo@laplateforme.io>
2025-05-13 11:05:11 +0200

    Add AdminProductController with CRUD methods

---
+++ app/controllers/AdminProductController.php
@@ -0,0 +1,41 @@
+<?php
+namespace App\Controllers;
+
+use App\Models\Product;
+
+class AdminProductController {
+    public function index() {
+        $productModel = new Product();
+        $products = $productModel->getAll();
+        require_once 'app/views/admin/products/index.php';
+    }
+
+    public function create() {
+        require_once 'app/views/admin/products/create.php';
+    }
+
+    public function store() {
+        // Récupérer et valider les données POST
+        $productModel = new Product();
+        $productModel->create($_POST);
+        header('Location: /admin/products');
+    }
+
+    public function edit($id) {
+        $productModel = new Product();
+        $product = $productModel->findById($id);
+        require_once 'app/views/admin/products/edit.php';
+    }
+
+    public function update($id) {
+        $productModel = new Product();
+        $productModel->update($id, $_POST);
+        header('Location: /admin/products');
+    }
+
+    public function delete($id) {
+        $productModel = new Product();
+        $productModel->delete($id);
+        header('Location: /admin/products');
+    }
+}
