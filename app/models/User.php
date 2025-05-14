546bdbae55510e7115fad34cc37f8d58c1ff2b25
esteban-genty <esteban.genty@laplateforme.io>
2025-05-14 15:44:02 +0200

    Adding files for product management in the back office

--- app/models/User.php
+++ app/models/User.php
@@ -9,7 +9,7 @@ class User
   public function __construct()
   {
     try {
-      $this->db = new \PDO('mysql:host=localhost;dbname=omni', 'root', 'root');
+      $this->db = new \PDO('mysql:host=localhost;dbname=omni', 'root', '');
       $this->db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
     } catch (\PDOException $e) {
       die('Erreur de connexion à la base de données : ' . $e->getMessage());
