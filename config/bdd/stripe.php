18b60a4b899b067f1abbe0508552a2b383b9a9ff
sebastien-liveyupeng <sebastien.live-yu-peng@laplateforme.io>
2025-05-15 14:28:49 +0200

    delete stripe files

--- config/bdd/stripe.php
+++
@@ -1,7 +0,0 @@
-<?php 
-// config/stripe.php
-return [
-    'secret_key' => 'sk_test_51ROaOm2U29I0bBmrL2mdDfQg0OCHOJV9eMkahXTGRIixYNOnQst7cfE4H5smwPQqU1GhxiM7ToIjKim1E6j8uZM2009lpM9rOA', 
-    'publishable_key' => 'pk_test_51ROaOm2U29I0bBmrNSbVhXvPQ74Ph4t3HfNZMql9qECFPYKxHm1LQyyo4akra5IsHpClc9fT07R5pQ7lXrI2Yrg8001D8vHg8k',  
-];
-
